53c8ef740cd2ae87e4afaab854de7a13753d3739
dbaty <dbaty@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-29 13:47:24 +0000

    Cosmectics.
    git-svn-id: https://svn.plone.org/svn/collective/Products.CMFNotification/trunk@229577 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- Products/CMFNotification/__init__.py
+++ Products/CMFNotification/__init__.py
@@ -3,11 +3,13 @@
 $Id$
 """
 from Products.CMFCore import utils as CMFCoreUtils
+
+## Apply monkey patches and set permissions
 import Products.CMFNotification.patches
-import permissions
+import Products.CMFNotification.permissions
 
 def initialize(context):
-    import NotificationTool
+    from Products.CMFNotification import NotificationTool
     tools = (NotificationTool.NotificationTool, )
     CMFCoreUtils.ToolInit(NotificationTool.META_TYPE,
                           tools=tools,
